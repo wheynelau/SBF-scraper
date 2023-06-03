16cda692282c5a45e2cbced1f27da9da48ccbc40
Wayne <waynelau15045@gmail.com>
2023-06-03 11:56:35 +0800

    refactor(main): rename mainflow.py to run.py
    Renamed the "mainflow.py" file to "run.py" to provide a more descriptive and intuitive name for the main execution file.

--- mainflow.py
+++
@@ -1,9 +0,0 @@
-import argparse
-from src import SBFScraper
-
-if __name__ == "__main__":
-    # get file naem from args
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", help="Name of file to save to")
-    args = parser.parse_args()
-    SBFScraper(filename=args.f).run()
